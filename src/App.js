da717778219f7c185b72b7f447f865a2e18b54f0
顾云翔 <guyunxiang32@gmail.com>
2020-11-20 15:44:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -16,23 +16,54 @@ import {
   ThemeProvider,
 } from '@material-ui/core/styles';
 
+import LogoImg from './assets/logo_title.svg';
+
 import { get } from './services/fetch';
 
 import './App.css';
 
 const theme = createMuiTheme({
-
+  palette: {
+    primary: {
+      main: '#ff7828',
+      contrastText: '#fff',
+    },
+  }
 });
 
 class App extends React.Component {
 
   state = {
+    sticky: false,
+    footerVisible: false,
+    activityKey: '',
     tagList: [],
     navList: [],
   }
 
   componentDidMount() {
+    this.getTagList();
     this.fetchData();
+
+    const { hash } = window.location;
+    if (hash) {
+      this.setState({
+        activityKey: decodeURIComponent(hash.split('#')[1]),
+      });
+    }
+
+    window.addEventListener("scroll", () => {
+      const { sticky } = this.state;
+      if (document.documentElement.scrollTop > 157 && !sticky) {
+        this.setState({
+          sticky: true,
+        });
+      } else if (document.documentElement.scrollTop < 157 && sticky) {
+        this.setState({
+          sticky: false,
+        });
+      }
+    });
   }
 
   fetchData = async () => {
@@ -40,28 +71,23 @@ class App extends React.Component {
     const res = await get('https://expo-res.sparkpool.com/SparkPoolOfficial/eth123.org/main/resource.json');
     if (res && (res || []).length) {
       try {
-        const tagList = this.getTagList(res);
         this.setState({
           navList: res,
-          tagList,
+          footerVisible: true,
         });
       } catch (e) {
       }
     }
   }
 
-  getTagList = (navList) => {
-    let back = [];
-    navList.forEach((item) => {
-      if (item.tags) {
-        item.tags.split(',').forEach(tag => {
-          if (back.indexOf(tag) < 0) {
-            back.push(tag);
-          }
-        });
-      }
-    });
-    return back;
+  getTagList = async (navList) => {
+    // const res = await get('/tagList.json');
+    const res = await get('https://expo-res.sparkpool.com/SparkPoolOfficial/eth123.org/main/tagList.json');
+    if (res && (res || []).length) {
+      this.setState({
+        tagList: res,
+      });
+    }
   }
 
   renderNavCard = (tag) => {
@@ -103,85 +129,156 @@ class App extends React.Component {
     return node;
   }
 
+  renderHotCard = () => {
+    const { navList, tagList } = this.state;
+    let node = [];
+    navList.forEach((item, index) => {
+      if (item.tags.indexOf('Home') > -1) {
+        node.push(
+          <Grid item xs={3} key={index}>
+            <Link
+              color="textPrimary"
+              href={item.website}
+              underline='none'>
+              <Box
+                bgcolor="white"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                className="hotCard"
+                py={2}
+                px={2}
+                borderRadius={8}
+              >
+                <img
+                  src={item.logo}
+                  alt=""
+                />
+              </Box>
+            </Link>
+          </Grid>
+        )
+      }
+    });
+    return (
+      <Box mb={2} id={(tagList[0] || {}).tag}>
+        <Grid container spacing={2}>
+          {node}
+        </Grid>
+      </Box>
+    );
+  }
+
+  renderNavContentCard = () => {
+    const { tagList } = this.state;
+    return (tagList || []).filter((item) => item.tag_en !== 'Home').map(({ tag, tag_en}) => (
+      <Box
+        bgcolor="white"
+        borderRadius={16}
+        mb={2}
+        key={tag}
+        id={tag}
+        px={3}
+      >
+        <Box
+          py={2}
+          border={1}
+          borderTop={0}
+          borderRight={0}
+          borderLeft={0}
+          borderColor="grey.100"
+        >
+          <Typography>{tag}</Typography>
+        </Box>
+        <Box py={3}>
+          <Grid container spacing={1}>
+            { this.renderNavCard(tag) }
+          </Grid>
+        </Box>
+      </Box>
+    ))
+  }
+
   render() {
 
-    const { tagList } = this.state;
+    const { tagList, sticky, footerVisible, activityKey } = this.state;
 
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Container>
           <Box mt={2} display="flex" flexDirection="column" alignItems="flex-end">
-            {/* <Select value="zh">
-              <MenuItem value="zh">简体中文</MenuItem>
-              <MenuItem value="en">English</MenuItem>
-            </Select> */}
             <Button>简体中文</Button>
           </Box>
           <Box mb={4} display="flex" flexDirection="column" alignItems="center">
             <Box mb={1}>
-              <Typography variant="h4">
-                ETH123.org
+              <img src={LogoImg} alt="eth123" />
+            </Box>
+            <Box>
+              <Typography color="textSecondary">
+                -- 以太坊生态项目导航 --
               </Typography>
             </Box>
-            <Typography>
-              以太坊生态项目导航
-            </Typography>
           </Box>
           <Box display="flex" flexDirection="row">
-            <Box mr={2} px={1} className="tagNav" id="tagNav">
+            <Box
+              mr={2}
+              px={1}
+              className={sticky ? "tagNav tagNav_fixed" : "tagNav"}
+              id="tagNav">
               {
-                (tagList || []).map((tag, index) => (
+                (tagList || []).map(({ tag, tag_en }, index) => (
                   <Link
                     color="textPrimary"
                     href={`#${tag}`}
                     key={tag}
-                    className={ index === 1 ? "tagLink tagLink_active" : 'tagLink'}
+                    className={ activityKey === tag ? "tagLink tagLink_active" : 'tagLink'}
                     underline="none"
+                    onClick={() => {
+                      this.setState({
+                        activityKey: tag,
+                      });
+                    }}
                   >
                     <Box py={1} px={2}>{tag}</Box>
                   </Link>
                 ))
               }
             </Box>
-            <Box flex={1}>
-              {
-                (tagList || []).map((tag) => (
-                  <Box
-                    bgcolor="white"
-                    borderRadius={16}
-                    mb={2}
-                    key={tag}
-                    id={tag}
-                    px={3}
-                  >
-                    <Box
-                      py={2}
-                      border={1}
-                      borderTop={0}
-                      borderRight={0}
-                      borderLeft={0}
-                      borderColor="grey.100"
-                    >
-                      <Typography>{tag}</Typography>
-                    </Box>
-                    <Box py={3}>
-                      <Grid container spacing={1}>
-                        { this.renderNavCard(tag) }
-                      </Grid>
-                    </Box>
-                  </Box>
-                ))
-              }
+            <Box flex={1}></Box>
+            <Box className="tagContent">
+              {this.renderHotCard()}
+              {this.renderNavContentCard()}
             </Box>
-          </Box>
-          <Box display="flex" flexDirection="column" alignItems="center" mt={2} mb={5}>
-            <Box mb={1}>
-              <Typography>一起发现更多新产品</Typography>
-            </Box>
-            <Button variant="contained" color="primary">提交</Button>
           </Box>
         </Container>
+        {
+          footerVisible ? (
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              className="footer"
+              py={4}>
+              <Box mb={3}>
+                <Typography variant="h5" className="footer_title">
+                  一起发现更多新产品
+                </Typography>
+              </Box>
+              <Button
+                variant="contained"
+                className="submitBtn"
+                color="primary"
+                onClick={() => {
+                  window.open('https://github.com/SparkPoolOfficial/eth123.org/issues');
+                }}
+                >
+                提交
+              </Button>
+            </Box>
+          ) : null
+        }
       </ThemeProvider>
     );
 
